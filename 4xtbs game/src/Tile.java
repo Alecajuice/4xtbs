854407c10e5cc6a52c6da444f93ac5c176e032bb
Alex Tarng <alecadad@gmail.com>
2014-01-30 21:14:29 -0800

    Added tile type and feat constants

--- 4xtbs game/src/Tile.java
+++ 4xtbs game/src/Tile.java
@@ -15,22 +15,37 @@ public class Tile implements ImageObserver
 	private final int HEIGHT = 200;
 	private final BufferedImage TILESHEET = ImageIO.read(new File("Tiles.png"));
 	
+	//Type constants
+	private final int TYPE_WATER = 1;
+	private final int TYPE_DESERT = 2;
+	private final int TYPE_PLAINS = 3;
+	private final int TYPE_TUNDRA = 4;
+	private final int TYPE_ROCK = 5;
+	
+	//Feature constants
+	private final int FEAT_NOFEAT = 0;
+	private final int FEAT_FOREST = 1;
+	private final int FEAT_MOUNTAIN = 2;
+	
 	private BufferedImage tileImage;
 	//Constructor instance variables
 	private Coordinate position;
 	private int ID;
 	private int building;
 	private int resource;
+	private int feature;
 	
 	//Tile constructor
-	public Tile(int iD, int build, Coordinate pos, int resourceIn) throws IOException
+	public Tile(int iD, int build, Coordinate pos, int resourceIn, int featureIn) throws IOException
 	{
 		ID = iD;
 		building = build;
 		position = pos;
 		resource = resourceIn;
+		feature = featureIn;
 		tileImage = TILESHEET.getSubimage(X_OFFSET*ID + WIDTH*(ID), Y_OFFSET, WIDTH, HEIGHT);
 	}
+
 
 	//Draw tiles
 	public void draw(Graphics screen)
@@ -73,11 +88,21 @@ public class Tile implements ImageObserver
 		return HEIGHT;
 	}
 
-	public int getResource() {
+	public int getResource() 
+	{
 		return resource;
 	}
 
-	public void setResource(int resource) {
+	public void setResource(int resource) 
+	{
 		this.resource = resource;
+	}
+	public int getFeature() 
+	{
+		return feature;
+	}
+	public void setFeature(int feature) 
+	{
+		this.feature = feature;
 	}
 }
