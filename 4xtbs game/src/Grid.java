eb7fab786fc2048e42e8cbe7ef8db1bf2ab1b2f6
Ben Cohen-Wang <ben.cohenwang@gmail.com>
2014-02-05 13:01:42 -0800

    Don't worry about it!

--- 4xtbs game/src/Grid.java
+++ 4xtbs game/src/Grid.java
@@ -1,124 +1,159 @@
-import java.awt.AWTEvent;
+import java.awt.Graphics;
 import java.io.IOException;
-
-
 public class Grid 
 {
-	private int width = 100;
-	private int height = 100;
-	private int ID;
-	private Tile[][] grid = new Tile[width][height];
-	public Grid(int n, int w, int h)
-	{
-		height = h;
-		width = w;
-		ID = n;
-	}
-	public Tile getTile(int x, int y)
-	{
-		return grid[x][y];
-	}
-	
-	public static double[] generateProbabilities(int TileID)
-	{
-		//TODO: Generate probability of adjacent Tiles depending on the ID of the base Tile
-		return null;
-	}
-	
-	public void generate() throws IOException
-	{
-		double[] probability = new double[6];
-		for(int i = 2; i < probability.length; i++)
-		{
-			probability[i] = 0.1;
-		}
-		probability[1] = 0.3;
-		probability[ID] = 0.4;
-		for(int i = 0; i < grid.length; i++)
-		{
-			for(int j = 0; j < grid[0].length; j++)
-			{
-				int random = (int)(101*Math.random());
-				int id = 0;
-				if(random >= 0 && random < probability[1])
-				{
-					id = 1;
-				}
-				if(random >= probability[1] && random < probability[1] + probability[2])
-				{
-					id = 2;
-				}
-				if(random >= probability[1] + probability[2] && random < probability[1] + probability[2] + probability[3])
-				{
-					id = 3;
-				}
-				if(random >= probability[1] + probability[2] + probability[3] && random < probability[1] + probability[2] + probability[3] + probability[4])
-				{
-					id = 4;
-				}
-				if(random >= probability[1] + probability[2] + probability[3] + probability[4] && random < probability[1] + probability[2] + probability[3] + probability[4] + probability[5])
-				{
-					id = 5;
-				}
-				grid[i][j] = new Tile(id, 0, new Coordinate(i,j), 0, 0);
-			}
-		}
-	}
-	//Getters and setters
-	public int getWidth() 
-	{
-		return width;
-	}
-	public void setWidth(int width) 
-	{
-		this.width = width;
-	}
-	public int getHeight() 
-	{
-		return height;
-	}
-	public void setHeight(int height) 
-	{
-		this.height = height;
-	}
-	public Tile[][] getGrid() 
-	{
-		return grid;
-	}
-	public void setGrid(Tile[][] grid) 
-	{
-		this.grid = grid;
-	}
-	public void setTile(int x, int y, Tile n)
-	{
-		grid[x][y] = n;
-	}
-	public void smooth()
-	{
-		for(int i = 0; i < grid.length; i++)
-		{
-			for(int j = 0; j < grid[0].length; j++)
-			{
-				int [] prob = new int[5];
-				for(int k = Math.max(0, i - 1); k <= Math.min(grid.length, i + 1); k++)
-				{
-					for(int l = Math.max(0, j - 1); l <= Math.min(grid[0].length, j + 1); l++)
-					{
-						if(!(k == i && l == j))
-						{
-							prob[grid[k][l].getID()] += 1;
-						}
-					}
-				}
-				int max = 0;
-				for(int a = 0; a < prob.length; a++)
-				{
-					if(prob[a] > max)
-					{
-						max = prob[a];
-					}
-				}
-			}
-		}
-	}
+    private int width = 100;
+    private int height = 100;
+    private int ID;
+    private Tile[][] grid = new Tile[width][height];
+    public Grid(int n, int w, int h) throws IOException
+    {
+        height = h;
+        width = w;
+        ID = n;
+        generate();
+    }
+    public Tile getTile(int x, int y)
+    {
+        return grid[x][y]; 
+    }
+    public void generate() throws IOException
+    {
+        double[] probability = new double[6];
+        for(int i = 2; i < probability.length; i++)
+        {
+            probability[i] = 0.2;
+        }
+        probability[1] = 0.2;
+        probability[ID] = 0.2;
+        for(int i = 0; i < grid.length; i++)
+        {
+            for(int j = 0; j < grid[0].length; j++)
+            {
+                double random = (Math.random());
+                int id = 0;
+                if(random >= 0 && random < probability[1])
+                {
+                    id = 1;
+                }
+                if(random >= probability[1] && random < probability[1] + probability[2])
+                {
+                    id = 2;
+                }
+                if(random >= probability[1] + probability[2] && random < probability[1] + probability[2] + probability[3])
+                {
+                    id = 3;
+                }
+                if(random >= probability[1] + probability[2] + probability[3] && random < probability[1] + probability[2] + probability[3] + probability[4])
+                {
+                    id = 4;
+                }
+                if(random >= probability[1] + probability[2] + probability[3] + probability[4] && random < probability[1] + probability[2] + probability[3] + probability[4] + probability[5])
+                {
+                    id = 5;
+                }
+                grid[i][j] = new Tile(id, 0, new Coordinate(i,j), 0, 0);
+            }
+        }
+    }
+    //Getters and setters
+    public int getWidth() 
+    {
+        return width;
+    }
+    public void setWidth(int width) 
+    {
+        this.width = width;
+    }
+    public int getHeight() 
+    {
+        return height;
+    }
+    public void setHeight(int height) 
+    {
+        this.height = height;
+    }
+    public Tile[][] getGrid() 
+    {
+        return grid;
+    }
+    public void setGrid(Tile[][] grid) 
+    {
+        this.grid = grid;
+    }
+    public void setTile(int x, int y, Tile n)
+    {
+        grid[x][y] = n;
+    }
+    public void draw(Graphics screen)
+    {
+        for(int i = 0; i < grid.length; i ++)
+        {
+            for(int j = 0; j < grid[0].length; j++)
+            {
+                grid[i][j].draw(screen);
+            }
+        }
+    }
+    public void smooth()
+    {
+        for(int i = 0; i < grid.length; i++)
+        {
+            for(int j = 0; j < grid[0].length; j++)
+            {
+                float [] prob = new float[5];
+                for(int k = Math.max(0, i - 1); k <= Math.min(grid.length, i + 1); k++)
+                {
+                    for(int l = Math.max(0, j - 1); l <= Math.min(grid[0].length, j + 1); l++)
+                    {
+                        prob[grid[k][l].getID()] += 1;
+                    }
+                }
+                float sum = 0;
+                for(int a = 0; a < prob.length; a++)
+                {
+                    sum += prob[a];
+                }
+                double r = Math.random();
+                for(int y = 0; y < prob.length; y++)
+                {
+                    int s = 0;
+                    for(int z = 0; z < y; z++)
+                    {
+                        s += z;
+                    }
+                    if(r > s && r < s + prob[y])
+                    {
+                        grid[i][j].setID(i);
+                    }
+                }
+//                Below is long version of above loop.
+//                If loop doesn't work, use below as model to fix it.
+//                if(r > 0 && r <= prob[0]/sum)
+//                {
+//                    grid[i][j].setID(0);
+//                }
+//                if(r > prob[0]/sum && r <= (prob[0] + prob[1])/sum)
+//                {
+//                    grid[i][j].setID(1);
+//                }
+//                if(r > (prob[0] + prob[1])/sum && r <= (prob[0] + prob[1] + prob[2])/sum)
+//                {
+//                    grid[i][j].setID(2);
+//                } 
+//                if(r > (prob[0] + prob[1] + prob[2])/sum && r <= (prob[0] + prob[1] + prob[2] + prob[3])/sum)
+//                {
+//                    grid[i][j].setID(3);
+//                }
+//                if(r > (prob[0] + prob[1] + prob[2] + prob[3])/sum && r <= (prob[0] + prob[1] + prob[2] + prob[3] + prob[4])/sum)
+//                {
+//                    grid[i][j].setID(4);
+//                }
+//                if(r > (prob[0] + prob[1] + prob[2] + prob[3] + prob[4])/sum && r <= (prob[0] + prob[1] + prob[2] + prob[3] + prob[4] + prob[5])/sum)
+//                {
+//                    grid[i][j].setID(5);
+//                }
+            }
+        }
+    }
 }
--- 4xtbs game/src/Main.java
+++ 4xtbs game/src/Main.java
@@ -12,12 +12,13 @@ import javax.swing.JPanel;
 
 public class Main extends JPanel implements KeyListener, MouseListener, MouseMotionListener
 {
-	private static String gameName = "4XTBS";
-	private static Grid map = new Grid(1, 100, 100);
+    private static String gameName = "4XTBS";
+    private static Grid map;
     static int screenWidth = 500;
     static int screenHeight = 500;
-	public static void main(String[] args) throws IOException
-	{
+    public static void main(String[] args) throws IOException
+    {
+        map = new Grid(2, 100, 100);
         JFrame frame = new JFrame();
         frame.setSize(screenWidth, screenHeight);
         frame.setTitle(gameName);
@@ -31,146 +32,55 @@ public class Main extends JPanel implements KeyListener, MouseListener, MouseMot
         frame.addMouseListener(drawer);
         frame.addMouseMotionListener(drawer);
         map.generate();
+        map.smooth();
         frame.repaint();
-        alexTest();
-	}
-	public static void alexTest()
-	{
-		System.out.println("calcDmg (Method 1) Test Cases: Calculate damage done to both Units when one Unit attacks an enemy Units");
-		int success1 = 0;
-		System.out.println("Test 1: (5, 5) --> {40, 40}");
-		int[] array1 = {40, 40};
-		if (Unit.calcDmg(5, 5) == array1)
-		{
-			System.out.println("Test succeeded");
-			success1++;
-		} else {
-			System.out.println("Test failed");
-		}
-		System.out.println("Test 2: (10, 5) --> {5, 75}");
-		int[] array2 = {5, 75};
-		if (Unit.calcDmg(10, 5) == array2)
-		{
-			System.out.println("Test succeeded");
-			success1++;
-		} else {
-			System.out.println("Test failed");
-		}
-		System.out.println("Test 3: (20, 5) --> {5, 100}");
-		int[] array3 = {5, 100};
-		if (Unit.calcDmg(20, 5) == array3)
-		{
-			System.out.println("Test succeeded");
-			success1++;
-		} else {
-			System.out.println("Test failed");
-		}
-		System.out.println("Percent of test cases succeeded: " + success1/3*100);
-		
-		System.out.println("calcRangeDmg (Method 2) Test Cases: Calculate damage done to a Units when a ranged Unit attacks an enemy Units");
-		int success2 = 0;
-		System.out.println("Test 1: (5, 5) --> 40");
-		if (Unit.calcRangeDmg(5, 5) == 40)
-		{
-			System.out.println("Test succeeded");
-			success2++;
-		} else {
-			System.out.println("Test failed");
-		}
-		System.out.println("Test 2: (5, 10) --> 20");
-		if (Unit.calcRangeDmg(10, 5) == 20)
-		{
-			System.out.println("Test succeeded");
-			success2++;
-		} else {
-			System.out.println("Test failed");
-		}
-		System.out.println("Test 3: (10, 5) --> 80");
-		if (Unit.calcRangeDmg(20, 5) == 80)
-		{
-			System.out.println("Test succeeded");
-			success2++;
-		} else {
-			System.out.println("Test failed");
-		}
-		System.out.println("Percent of test cases succeeded: " + success2/3*100);
-
-		System.out.println("generateProbabilities (Method 3) Test Cases: Generate probability of adjacent Tiles depending on the ID of the base Tile");
-		int success3 = 0;
-		double[] array4 = {0.8, 0.05, 0.05, 0.05, 0.05};
-		System.out.println("Test 1: (1) --> 40");
-		if (Grid.generateProbabilities(1) == array4)
-		{
-			System.out.println("Test succeeded");
-			success3++;
-		} else {
-			System.out.println("Test failed");
-		}
-		double[] array5 = {0.25, 0.05, 0.6, 0.05, 0.05};
-		System.out.println("Test 2: (3) --> 20");
-		if (Grid.generateProbabilities(3) == array5)
-		{
-			System.out.println("Test succeeded");
-			success3++;
-		} else {
-			System.out.println("Test failed");
-		}
-		double[] array6 = {0.25, 0.05, 0.05, 0.05, 0.6};
-		System.out.println("Test 3: (5) --> 80");
-		if (Grid.generateProbabilities(5) == array6)
-		{
-			System.out.println("Test succeeded");
-			success3++;
-		} else {
-			System.out.println("Test failed");
-		}
-		System.out.println("Percent of test cases succeeded: " + success3/3*100);
-	}
-	public void paintComponent(Graphics screen)
-	{
-		
-	}
-	//Mouse events
-	public void mouseDragged(MouseEvent arg0) 
-	{
-		
-	}
-	public void mouseMoved(MouseEvent arg0) 
-	{
-		
-	}
-	public void mouseClicked(MouseEvent arg0) 
-	{
-		
-	}
-	public void mouseEntered(MouseEvent arg0) 
-	{
-		
-	}
-	public void mouseExited(MouseEvent arg0) 
-	{
-		
-	}
-	public void mousePressed(MouseEvent arg0) 
-	{
-		
-	}
-	public void mouseReleased(MouseEvent arg0) 
-	{
-		
-	}
-	
-	//Keyboard Events
-	public void keyPressed(KeyEvent arg0) 
-	{
-		
-	}
-	public void keyReleased(KeyEvent arg0) 
-	{
-		
-	}
-	public void keyTyped(KeyEvent arg0) 
-	{
-		
-	}
+    }
+    public void paintComponent(Graphics screen)
+    {
+        map.draw(screen);
+    }
+    
+    //Mouse events
+    public void mouseDragged(MouseEvent arg0) 
+    {
+        
+    }
+    public void mouseMoved(MouseEvent arg0) 
+    {
+        
+    }
+    public void mouseClicked(MouseEvent arg0) 
+    {
+        
+    }
+    public void mouseEntered(MouseEvent arg0) 
+    {
+        
+    }
+    public void mouseExited(MouseEvent arg0) 
+    {
+        
+    }
+    public void mousePressed(MouseEvent arg0) 
+    {
+        
+    }
+    public void mouseReleased(MouseEvent arg0) 
+    {
+        
+    }
+    
+    //Keyboard Events
+    public void keyPressed(KeyEvent arg0) 
+    {
+        
+    }
+    public void keyReleased(KeyEvent arg0) 
+    {
+        
+    }
+    public void keyTyped(KeyEvent arg0) 
+    {
+        
+    }
 }
