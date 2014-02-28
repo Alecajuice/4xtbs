069d274a742cfdddcddc12536a4fc192b3619775
Alex Tarng <alecadad@gmail.com>
2014-02-27 16:40:06 -0800

    test1

--- 4xtbs game/src/Main.java
+++ 4xtbs game/src/Main.java
@@ -12,6 +12,7 @@ import javax.swing.JPanel;
 
 public class Main extends JPanel implements KeyListener, MouseListener, MouseMotionListener
 {
+	//test
 	public static double zoomRatio = 0.25;
 	//test
     private static String gameName = "4XTBS";
